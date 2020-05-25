cff6a203e2767eaee7427bed4818b684d08641de
Rohan Varma <rohan@prompthire.com>
2020-05-25 13:01:43 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This is our chrome extension in react
+    </div>
+  );
+}
+
+export default App;
